53bf66a65deeb70c27554845157b57ddec919daf
Omri Levy <omri.levy0197@gmail.com>
2020-08-16 13:12:32 +0300

    now changing classLists only if the target is not null to avoid errors

--- client/src/static/js/handlers.js
+++ client/src/static/js/handlers.js
@@ -2,38 +2,38 @@ const displayTaskOptionsTooltip = () => {
     const selector = '.task-options-tooltip-container'
     const parent = document.querySelector(selector);
     const target = parent.firstChild;
-    target.classList.remove('hidden');
+    target && target.classList.remove('hidden');
 }
 
 const hideTaskOptionsTooltip = () => {
     const selector = '.task-options-tooltip-container'
     const parent = document.querySelector(selector);
     const target = parent.firstChild;
-    target.classList.add('hidden');
+    target && target.classList.add('hidden');
 }
 
 const displayTaskFilterTooltip = (Event) => {
     const parent = Event.target;
     const target = parent.firstChild.firstChild;
-    target.classList.remove('hidden');
+    target && target.classList.remove('hidden');
 }
 const hideTaskFilterTooltip = (Event) => {
     const parent = Event.target;
     const target = parent.firstChild.firstChild;
-    target.classList.add('hidden');
+    target && target.classList.add('hidden');
 }
 
 const toggleCreateTaskModal = () => {
     const selector = '.create-task-modal-container';
-    const createTaskModal = document.querySelector(selector);
+    const target = document.querySelector(selector);
 
-    createTaskModal.classList.toggle('hidden');
+    target && target.classList.toggle('hidden');
 }
 
 const toggleEditTaskModal = () => {
     const selector = '.edit-task-modal-container';
     const target = document.querySelector(selector);
-    target.classList.toggle('hidden');
+    target && target.classList.toggle('hidden');
 }
 
 const editTask = (id, setEditTaskId) => {
