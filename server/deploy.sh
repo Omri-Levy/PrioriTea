ff341af23e7cb65a12685f6c9b8348b9f28a0d7f
Omri Levy <omri.levy0197@gmail.com>
2020-08-31 15:18:06 +0300

    made sh scripts to make my life easier

--- server/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Image version?
-read -r VERSION
-sudo docker build -t omrilevy0197/prioritea:"$VERSION" .
-sudo docker push omrilevy0197/prioritea:"$VERSION"
-ssh root@188.166.60.237 "docker pull omrilevy0197/prioritea:$VERSION && docker tag omrilevy0197/prioritea:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
