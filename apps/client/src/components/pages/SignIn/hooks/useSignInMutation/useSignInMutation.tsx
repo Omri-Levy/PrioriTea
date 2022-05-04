b44c29d78b695770dd3e8bbb0ca9f04233c63a57
Omri Levy <omri.levy0197@gmail.com>
2022-05-04 16:48:51 +0300

    added loading to sign in

--- apps/client/src/components/pages/SignIn/hooks/useSignInMutation/useSignInMutation.tsx
+++ apps/client/src/components/pages/SignIn/hooks/useSignInMutation/useSignInMutation.tsx
@@ -1,12 +1,23 @@
-import { useMutation } from "react-query";
-import { AuthApi } from "../../../../../api/auth-api";
-import { ISignInForm } from "../../interfaces";
+import {useMutation, useQueryClient} from "react-query";
+import {AuthApi} from "../../../../../api/auth-api";
+import {ISignInForm} from "../../interfaces";
+
+export const asyncTimeout = async (ms = 0) =>
+	new Promise((resolve) => setTimeout(resolve, ms)
+	);
 
 export const useSignInMutation = () => {
+	const queryClient = useQueryClient();
+
   return useMutation(
     ["signUp"],
     async ({ email, password }: ISignInForm) => {
       return AuthApi.signIn(email, password);
-    }
+    },
+	  {
+		  onSuccess: () => {
+			  queryClient.invalidateQueries(['userInfo']);
+		  }
+	  }
   );
 };
