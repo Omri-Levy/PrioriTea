f930c8d16177ce01509444089cd1fae43a0a025a
Omri Levy <omri.levy0197@gmail.com>
2022-05-04 16:51:52 +0300

    added loading to sign up

--- apps/client/src/components/pages/SignUp/hooks/useSignUpMutation/useSignUpMutation.tsx
+++ apps/client/src/components/pages/SignUp/hooks/useSignUpMutation/useSignUpMutation.tsx
@@ -1,11 +1,13 @@
-import { useMutation } from "react-query";
-import { AuthApi } from "../../../../../api/auth-api";
-import { ISignUpForm } from "../../interfaces";
+import {useMutation} from "react-query";
+import {AuthApi} from "../../../../../api/auth-api";
+import {ISignUpForm} from "../../interfaces";
 
 export const useSignUpMutation = () => {
   return useMutation(
     ["signUp"],
-    async ({ email, name, password, passwordConfirmation }: ISignUpForm) => {
+    async (
+		{ email, name, password, passwordConfirmation }: ISignUpForm
+	) => {
       return AuthApi.signUp(email, name, password, passwordConfirmation);
     }
   );
