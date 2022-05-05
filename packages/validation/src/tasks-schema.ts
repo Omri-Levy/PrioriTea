9f8e2d4e8058b5d7add95c4fcff1784f075f2ef5
Omri Levy <omri.levy0197@gmail.com>
2022-05-05 18:24:24 +0300

    updated the zod validation of the task priority

--- packages/validation/src/tasks-schema.ts
+++ packages/validation/src/tasks-schema.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const taskSchema = z.object({
 	id: z.string().cuid(),
-	priority: z.string().min(1).max(80),
+	priority: z.number().min(1, `Priority must be greater than or equal to 1`).max(5, `Priority must be less than or equal to 5`),
 	description: z.string().min(1).max(80),
 	status: z.string().min(1).max(80).optional(),
 	userId: z.string().cuid(),
